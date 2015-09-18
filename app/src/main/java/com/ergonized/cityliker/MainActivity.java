5aa1329664c006802f6fce6b208003e58dfba825
Evgen <bloodelf1985@gmail.com>
2015-09-18 15:39:53 +0300

    create MainActivity and add layout

---
+++ app/src/main/java/com/ergonized/cityliker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ergonized.cityliker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Evgen on 18.09.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
