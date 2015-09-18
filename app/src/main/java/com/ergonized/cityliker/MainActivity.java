5eacfdb8efcc3a212e6bdf1f008c15fb4be75aec
Evgen <bloodelf1985@gmail.com>
2015-09-18 16:11:28 +0300

    create toolbar

--- app/src/main/java/com/ergonized/cityliker/MainActivity.java
+++ app/src/main/java/com/ergonized/cityliker/MainActivity.java
@@ -2,15 +2,32 @@ package com.ergonized.cityliker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by Evgen on 18.09.2015.
- */
+
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
