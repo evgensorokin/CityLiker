f5a2b1f145ef745cabd0fdc3a3c28bae0c2e206b
Evgen <bloodelf1985@gmail.com>
2015-09-21 12:53:41 +0300

    add tab layout

--- app/src/main/java/com/ergonized/cityliker/MainActivity.java
+++ app/src/main/java/com/ergonized/cityliker/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ergonized.cityliker;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -54,5 +57,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_closed);
+        drawerLayout.setDrawerListener(toogle);
+        toogle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()){
+                    case R.id.actionMainPageItem:
+                        showNotificationTab();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void showNotificationTab() {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
